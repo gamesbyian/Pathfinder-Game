c7211668cdc8cafdd9de7ab5a79f17a267e126f2
gamesbyian <ianmakesjokes@gmail.com>
2026-08-27 15:20:26 -0600

    Update vitest.config.mjs for kebab-case test support

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -33,7 +33,7 @@ export default defineConfig({
             ],
             exclude: [
                 'modules/**/*.test.ts',
-                'modules/**/*.test-support.ts',
+                'modules/**/*-test-support.ts',
                 'modules/**/types.ts',
                 'modules/solver/testing-api.ts',
             ],
