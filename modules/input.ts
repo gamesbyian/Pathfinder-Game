db59ef88194d59b5239e2cff2e196c7e0b5f785b
gamesbyian <ianmakesjokes@gmail.com>
2026-08-31 13:49:26 -0600

    refactor: remove LevelUtils from input composition

--- modules/input.ts
+++ modules/input.ts
@@ -10,7 +10,7 @@ import { createLevelRatingController }    from './input/level-rating-controller.
 import { createFalseGoalTriggerScanController }       from './input/false-goal-trigger-scan-controller.js';
 import { setGamepadGridPrimaryAction }     from './state-actions.js';
 
-export function createInput({ state, ui, engine, levelUtils, editor, renderer, themes, data, devCorpus, solverApi, persistence, audioService, reportError }: any) {
+export function createInput({ state, ui, engine, editor, renderer, themes, data, devCorpus, solverApi, persistence, audioService, reportError }: any) {
     let initialized = false;
 
     const init = () => {
@@ -18,15 +18,15 @@ export function createInput({ state, ui, engine, levelUtils, editor, renderer, t
         initialized = true;
         setGamepadGridPrimaryAction(state, () => {});
 
-        const navController = createNavigationController({ state, ui, engine, levelUtils, editor, renderer });
-        createGamepadController({ state, ui, engine, levelUtils }, navController);
-        createPointerInputController({ state, ui, engine, levelUtils, editor, renderer });
-        createOptionsController({ state, ui, engine, themes, data, devCorpus, solverApi, levelUtils, persistence, audioService, reportError }, { tryNavigate: navController.tryNavigate });
-        const falseGoalTriggerScan = createFalseGoalTriggerScanController({ state, ui, engine, levelUtils, editor, solverApi, reportError });
-        createEditorToolbarController({ state, ui, engine, levelUtils, editor, solverApi, reportError }, { tryNavigate: navController.tryNavigate, falseGoalTriggerScan });
-        createSubmissionController({ state, ui, engine, levelUtils, editor, persistence, solverApi, data, reportError });
-        createReviewController({ state, ui, engine, levelUtils, editor, persistence, solverApi, reportError });
-        createSolverController({ state, ui, engine, levelUtils, solverApi, reportError });
+        const navController = createNavigationController({ state, ui, engine, data, editor, renderer });
+        createGamepadController({ state, ui, engine }, navController);
+        createPointerInputController({ state, ui, engine, editor, renderer });
+        createOptionsController({ state, ui, engine, themes, data, devCorpus, solverApi, persistence, audioService, reportError }, { tryNavigate: navController.tryNavigate });
+        const falseGoalTriggerScan = createFalseGoalTriggerScanController({ state, ui, engine, editor, solverApi, reportError });
+        createEditorToolbarController({ state, ui, engine, editor, solverApi, reportError }, { tryNavigate: navController.tryNavigate, falseGoalTriggerScan });
+        createSubmissionController({ state, ui, engine, editor, persistence, solverApi, data, reportError });
+        createReviewController({ state, ui, engine, editor, persistence, solverApi, reportError });
+        createSolverController({ state, ui, engine, solverApi, reportError });
         createLevelRatingController({ engine });
     };
 
