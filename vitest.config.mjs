9bb609c38c36ae4ff3111fd5c6d920c55c396245
gamesbyian <ianmakesjokes@gmail.com>
2026-09-24 16:36:34 -0600

    Allow threshold-free intermediate coverage shards

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
                 'modules/**/types.ts',
                 'modules/solver/testing-api.ts',
             ],
-            thresholds: {
+            thresholds: process.env.PATHFINDER_COVERAGE_SHARD === '1' ? undefined : {
                 statements: 82,
                 branches: 72,
                 functions: 90,
